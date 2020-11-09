0e867ff9996ef52a8374edf9017dec790945d704
Volker Sengler <git@sengler.de>
2020-11-09 21:02:10 +0100

    fix

--- main.js
+++ main.js
@@ -1537,7 +1537,7 @@ class VwWeconnect extends utils.Adapter {
                                     });
                                     // if (path === "status")
                                     //	adapter.log.info(this.key + " value = " + value + " of: " + modPath.join(".") + " ID = " + dataId + "/" + fieldId);
-                                    if (dataId == "0x030104FFFF" && fieldId == "0x0301040001" && this.key = "value") {
+                                    if (dataId == "0x030104FFFF" && fieldId == "0x0301040001" && this.key == "value") {
                                     	adapter.log.info('is car locked: ' + value + " yes/no " + (value == 2));
                                     	adapter.setState(vin + "." + path + ".isCarLocked", value == 2, true);
                                     }
