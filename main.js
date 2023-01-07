337777a6a5f78fef09ca1905780594c431066c5b
Volker Sengler <git@sengler.de>
2023-01-07 09:13:53 +0100

    test

--- main.js
+++ main.js
@@ -2393,6 +2393,7 @@ class VwWeconnect extends utils.Adapter {
         },
       })
         .then((res) => {
+          this.log.info("ParkingPosition resp = " + JSON.stringify(res));
           this.log.debug(JSON.stringify(res.data));
           this.extractKeys(this, vin + ".parkingposition", res.data.data);
         })
