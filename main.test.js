f4cf50cd5ec5fb324bdb583b4a1a071c2874c41f
TA2k <tombox2020@gmail.com>
2023-09-03 11:48:19 +0200

    fix json2iob lib

--- main.test.js
+++ main.test.js
@@ -13,18 +13,17 @@ const { expect } = require("chai");
 // import { functionToTest } from "./moduleToTest";
 
 describe("module to test => function to test", () => {
-	// initializing logic
-	const expected = 5;
+  // initializing logic
+  const expected = 5;
 
-	it(`should return ${expected}`, () => {
-		const result = 5;
-		// assign result a value from functionToTest
-		expect(result).to.equal(expected);
-		// or using the should() syntax
-		result.should.equal(expected);
-	});
-	// ... more tests => it
-
+  it(`should return ${expected}`, () => {
+    const result = 5;
+    // assign result a value from functionToTest
+    expect(result).to.equal(expected);
+    // or using the should() syntax
+    result.should.equal(expected);
+  });
+  // ... more tests => it
 });
 
 // ... more test suites => describe
