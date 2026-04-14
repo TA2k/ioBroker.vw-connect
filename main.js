5b634e0d6ef4b6ffdbae7287de51561f8cfd73ae
TA2k <tombox2020@gmail.com>
2026-04-14 11:53:15 +0200

    add more status fields for audi

--- main.js
+++ main.js
@@ -3012,7 +3012,7 @@ class VwWeconnect extends utils.Adapter {
         url:
           "https://emea.bff.cariad.digital/vehicle/v1/vehicles/" +
           vin +
-          "/selectivestatus?jobs=access,activeVentilation,auxiliaryHeating,batteryChargingCare,batterySupport,charging,chargingProfiles,climatisation,climatisationTimers,departureProfiles,fuelStatus,honkAndFlash,hybridCarAuxiliaryHeating,vehicleHealthWarnings,vehicleHealthInspection,vehicleLights,measurements,departureTimers",
+          "/selectivestatus?jobs=access,activeVentilation,auxiliaryHeating,batteryChargingCare,batterySupport,charging,chargingProfiles,chargingTimers,climatisation,climatisationTimers,departureProfiles,departureTimers,fuelStatus,honkAndFlash,hybridCarAuxiliaryHeating,lvBattery,measurements,oilLevel,readiness,vehicleHealthInspection,vehicleHealthWarnings,vehicleLights",
         headers: {
           "content-type": "application/json",
           accept: "*/*",
